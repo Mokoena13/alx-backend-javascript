de13baa1839892d0c0dbdb8cd1d2de5c089ea341
Mokoena13 <ernestmokoena.mokoena@gmail.com>
2024-03-07 05:25:04 -0800

    update

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
+"""
+0-pascal_triangle
+"""
+
+
 def pascal_triangle(n):
-
-    matrix = []
-
+    """
+    Returns a list of integers
+    representing the Pascal Triangle of n
+    returns empty list if n <= 0
+    """
+    k = []
     if n <= 0:
-        return matrix     
-    matrix = [[1]] 
-
-        for i in range(1, n):
-        temp = [1] 
-                for j in range(len(matrix[i - 1]) - 1):
-            curr = matrix[i - 1]
-            temp.append(matrix[i - 1][j] + matrix[i - 1][j + 1])
+        return k
+    k = [[1]]
+    for i in range(1, n):
+        temp = [1]
+        for j in range(len(k[i - 1]) - 1):
+            curr = k[i - 1]
+            temp.append(k[i - 1][j] + k[i - 1][j + 1])
         temp.append(1)
-        matrix.append(temp)
-    return matrix
+        k.append(temp)
+    return k
